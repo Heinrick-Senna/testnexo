777820bff532690145894c0fbd4be1835a00425e
Heinrick Senna <marcelohz198@hotmail.com>
2024-06-29 14:26:53 -0300

    Updating

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,8 @@ import emojis from './emojis';
 const router = express.Router();
 
 router.post<{}, any>('/add', async (req, res) => {
-  console.log(req.body);
+  console.log("This MotherFuck Body", req.body);
+  console.log("This MotherFuck REQ", req);
   return res.status(202).json({ success: true })
 });
 
